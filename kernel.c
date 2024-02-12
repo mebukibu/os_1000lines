76df7b62648cba8207763cfc534104c45b6ee413
mebukibu <masa1280g@gmail.com>
2024-02-12 11:44:38 +0900

    10 メモリ割り当てまで

--- kernel.c
+++ kernel.c
@@ -1,7 +1,21 @@
 #include "kernel.h"
 #include "common.h"
 
-extern char __bss[], __bss_end[], __stack_top[];
+extern char __stack_top[];
+extern char __bss[], __bss_end[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+  static paddr_t next_paddr = (paddr_t) __free_ram;
+  paddr_t paddr = next_paddr;
+  next_paddr += n * PAGE_SIZE;
+
+  if (next_paddr > (paddr_t) __free_ram_end)
+    PANIC("out of memory");
+  
+  memset((void *) paddr, 0, n * PAGE_SIZE);
+  return paddr;
+}
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid,  long eid) {
@@ -117,7 +131,13 @@ void kernel_main(void) {
 
   WRITE_CSR(stvec, (uint32_t) kernel_entry);
   printf("\n\nHello %s\n", "World!");
-  __asm__ __volatile__("unimp");
+
+  paddr_t paddr0 = alloc_pages(2);
+  paddr_t paddr1 = alloc_pages(1);
+  printf("alloc_pages test: paddr0=%x\n", paddr0);
+  printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+  PANIC("booted!");
 }
 
 __attribute__((section(".text.boot")))
