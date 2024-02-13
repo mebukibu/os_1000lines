aefe302b919cf4ad1f147d35e0a162d37dc0c424
mebukibu <masa1280g@gmail.com>
2024-02-13 13:17:13 +0900

    15 システムコール システムコールのテストまで

--- kernel.h
+++ kernel.h
@@ -6,6 +6,7 @@
 #define PROC_RUNNABLE 1
 #define SATP_SV32 (1u << 31)
 #define SSTATUS_SPIE (1 << 5)
+#define SCAUSE_ECALL 8
 #define PAGE_V    (1 << 0)
 #define PAGE_R    (1 << 1)
 #define PAGE_W    (1 << 2)
